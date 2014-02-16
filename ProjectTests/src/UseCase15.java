3c42b6ae59a893e81ae387174cb67864d6182f17
alexgfh <alexgfh@gmail.com>
2014-02-16 00:58:32 -0700

    Added more tests to the tests folder

---
+++ ProjectTests/src/UseCase15.java
@@ -0,0 +1,31 @@
+public void testSortByRank(ArrayList<Location> locationList) {
+    TopLevelCommentModel topComment1 = new TopLevelCommentModel("name1", locationList.get(1));
+    TopLevelCommentModel topComment2 = new TopLevelCommentModel("name2", locationList.get(2));
+    topComment2.increaseRating;
+    TopLevelCommentModel topComment3 = new TopLevelCommentModel("name3", locationList.get(3));
+    topComment3.increaseRating;
+    topComment3.increaseRating;
+    TopLevelCommentModel topComment4 = new TopLevelCommentModel("name4", locationList.get(4));
+    topComment4.increaseRating;
+    topComment4.increaseRating;
+    topComment4.increaseRating;
+    TopLevelCommentModel topComment5 = new TopLevelCommentModel("name5", locationList.get(5));
+    topComment5.increaseRating;
+    topComment5.increaseRating;
+    topComment5.increaseRating;
+    topComment5.increaseRating;
+    ArrayList<TopLevelCommentModel> arrayList1 = new ArrayList<TopLevelCommentModel>;
+    ArrayList<TopLevelCommentModel> arrayList2 = new ArrayList<TopLevelCommentModel>;
+    arrayList1.add(topComment2);
+    arrayList1.add(topComment1);
+    arrayList1.add(topComment5);
+    arrayList1.add(topComment4);
+    arrayList1.add(topComment3);
+    arrayList2.add(topComment1);
+    arrayList2.add(topComment2);
+    arrayList2.add(topComment3);
+    arrayList2.add(topComment4);
+    arrayList2.add(topComment5);
+    Sort.sortByRank(arrayList1, locationList.get(0));
+    assertTrue("The arraylists should be the same after the sort", (arrayList1 == arrayList2));
+}
