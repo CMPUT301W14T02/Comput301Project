8c9e1680e18cae0b5fd68590d5a4d5184d27b6f7
Alex <alexgfh@gmail.com>
2014-02-20 13:37:12 -0700

    Add UserModel and gitignore

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/UserModel.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/UserModel.java
@@ -2,4 +2,32 @@ package ca.ualberta.cs.cmput301t02project;
 
 public class UserModel {
 
+	String name;
+	ArrayList<FavoritesListModel> favoritesList;
+	ArrayList<MyCommentsModel> myComments;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public ArrayList<FavoritesListModel> getFavoritesList() {
+		return favoritesList;
+	}
+	
+	public void setFavoritesList(ArrayList<FavoritesListModel> favoritesList) {
+		this.favoritesList = favoritesList;
+	}
+	
+	public ArrayList<MyCommentsModel> getMyComments() {
+		return myComments;
+	}
+	
+	public void setMyComments(ArrayList<MyCommentsModel> myComments) {
+		this.myComments = myComments;
+	}
+	
 }
