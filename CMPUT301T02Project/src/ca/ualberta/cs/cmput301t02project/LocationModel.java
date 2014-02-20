08420083c9965d2c228dba1571d7d36662e55b0a
homynyk <homynyk@ualberta.ca>
2014-02-20 15:19:40 -0700

    cleaned up. added constructor

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/LocationModel.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/LocationModel.java
@@ -1,6 +1,5 @@
 package ca.ualberta.cs.cmput301t02project;
 
-
 import android.app.Application;
 import android.content.Context;
 import android.location.Location;
@@ -8,64 +7,74 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+public class LocationModel extends Application
+{
 
-public class LocationModel extends Application {
-	
-	double lat;
-	double lng;
-	Location location; 
-	
+	private double lat;
+	private double lng;
+	private Location location;
 
-	//LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	//lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
-	
-	//Retrieve location manager
-	
-	public void afunct(){
-	LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);	
-	//Requests location from provider
-	//String provider = LocationManager.GPS_PROVIDER;
-	
-	//locationManager.requestSingleUpdate(provider, locationListener, looper);
-	
-	locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-	
-	//if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){ 
-      //  locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-    //}
+	public LocationModel()
+	{
+
+		super();
+		// Retrieve location manager
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// Requests location from provider
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+				0, locationListener);
+
+	/*	MAY NEED TO IMPLEMENT THIS LATER FOR CACHED COMMENTS  
+	 *  
+	 	if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+		 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+		 0, 0, this);
+		 } */
 	}
 
 	// Retrieve location updates through LocationListener interface
-	private final LocationListener locationListener = new LocationListener(){
-	// TODO: override the four methods.
+	private final LocationListener locationListener = new LocationListener()
+	{
+
+		// TODO: override the four methods.
 		@Override
-		public void onLocationChanged (Location location) {
-			if (location != null) {
+		public void onLocationChanged(Location location)
+		{
+
+			if (location != null)
+			{
 				double lat = location.getLatitude();
 				double lng = location.getLongitude();
 
-				
-			} else {
-				//do something later here
+			} else
+			{
+				// do something later here
 			}
 		}
-		@Override
-		public void onProviderDisabled (String provider) {
-			//TODO
-		}
-		@Override
-		public void onProviderEnabled (String provider) {
-			//TODO
-		}
-		@Override
-		public void onStatusChanged (String provider, int status, Bundle extras) {
-			//TODO
-		}
-		
-	};
-	
 
-	//locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-	//Stores in location object
+		@Override
+		public void onProviderDisabled(String provider)
+		{
+
+			// TODO
+		}
+
+		@Override
+		public void onProviderEnabled(String provider)
+		{
+
+			// TODO
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras)
+		{
+
+			// TODO
+		}
+
+	};
+
+	// Stores in location object
 
 }
