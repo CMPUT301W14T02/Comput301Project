c460314f02d64c14d423e911ca975df877d6d8c8
Alex <alexgfh@gmail.com>
2014-02-20 16:15:42 -0700

    Added editText on CreateComment

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/CreateCommentActivity.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/CreateCommentActivity.java
@@ -1,8 +1,11 @@
 package ca.ualberta.cs.cmput301t02project;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateCommentActivity extends Activity {
 
@@ -11,6 +14,17 @@ public class CreateCommentActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_comment);
 		
+		EditText inputComment = (EditText) findViewById(R.id.create_text);
+		
+		Button post = (Button) findViewById(R.id.create_post);
+		
+		post.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//String comment = inputComment.getText().toString();
+			}
+		});
 	}
 
 	@Override
