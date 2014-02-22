2c7ed136c0628c22351d5d682be6f7fae591af67
kwicen <kwicento@ualberta.ca>
2014-02-22 14:52:47 -0700

    finished organizing use case tests

--- ProjectTests/src/BrowseCommentsActivityTest.java
+++ ProjectTests/src/BrowseCommentsActivityTest.java
@@ -111,5 +111,17 @@ public class BrowseCommentsActivityTest extends
   	}
   	
       // End of Use Case 11 Test
+  	
+  	// Use Case 21: View Commentor Username
+  	public void testDisplayUsername() {
+  	        Intent intent = new Intent();
+  	        String text = "ThisIsMyName";
+  	        intent.putExtra(BrowsingCommentsActivity.TEXT_KEY, text);
+  	        setActivityIntent(intent);
+  	        BrowsingCommentsActivity activity = getActivity();
+  	        TextView textView = (TextView)activity.findViewById(ca.ualberta.cs.project301.R.id.commenterUsername);
+  	        assertEquals("Username TextView should be visible", text, textView.getText().toString());
+  	    } 
+  	// End of Use Case 21 test
 
 }
