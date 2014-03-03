6d7a79ec4285eea9301b3c6171ba161e91e4dabb
Sasha Babicki <sbabicki@gmail.com>
2014-03-02 17:58:54 -0700

    changed layout for main activity

--- CMPUT301T02Project/gen/ca/ualberta/cs/cmput301t02project/R.java
+++ CMPUT301T02Project/gen/ca/ualberta/cs/cmput301t02project/R.java
@@ -24,21 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
-        public static final int browse=0x7f080005;
-        public static final int commentListView=0x7f08000d;
+        public static final int action_settings=0x7f080010;
+        public static final int browse=0x7f080007;
+        public static final int commentListView=0x7f08000f;
         public static final int create=0x7f080006;
         public static final int create_post=0x7f080001;
         public static final int create_text=0x7f080000;
-        public static final int fav_button=0x7f08000a;
+        public static final int fav_button=0x7f08000c;
+        public static final int favorites=0x7f080008;
         public static final int login_button=0x7f080002;
         public static final int login_message=0x7f080004;
         public static final int login_username=0x7f080003;
-        public static final int replyListView=0x7f08000c;
-        public static final int reply_button=0x7f080009;
-        public static final int selected_comment=0x7f080008;
-        public static final int spinner=0x7f08000b;
-        public static final int welcome_message=0x7f080007;
+        public static final int my_comments=0x7f080009;
+        public static final int replyListView=0x7f08000e;
+        public static final int reply_button=0x7f08000b;
+        public static final int selected_comment=0x7f08000a;
+        public static final int spinner=0x7f08000d;
+        public static final int welcome_message=0x7f080005;
     }
     public static final class layout {
         public static final int activity_create_comment=0x7f030000;
@@ -59,10 +61,12 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int browse=0x7f050005;
         public static final int create=0x7f050004;
+        public static final int favorites=0x7f050010;
         public static final int hello_world=0x7f050002;
         public static final int login_button=0x7f05000e;
         public static final int login_hint=0x7f05000d;
         public static final int login_message=0x7f05000c;
+        public static final int my_comments=0x7f050011;
         public static final int post=0x7f050007;
         public static final int spinner_title=0x7f050009;
         public static final int title_activity_create_comment=0x7f050006;
