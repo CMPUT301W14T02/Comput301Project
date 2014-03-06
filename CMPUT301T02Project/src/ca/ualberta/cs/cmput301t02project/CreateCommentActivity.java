9a330f215388f728536eabad5001d8808086f20e
kwicen <kwicento@ualberta.ca>
2014-03-06 16:32:29 -0700

    Moved location stuff into createComment. Still says it's null though.

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/CreateCommentActivity.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/CreateCommentActivity.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 import ca.ualberta.cs.cmput301t02project.model.CommentModel;
 import ca.ualberta.cs.cmput301t02project.model.LocationModel;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -17,10 +18,12 @@ import android.widget.EditText;
 
 public class CreateCommentActivity extends Activity {
 
-	//TODO: Refactor with new classes
-	//private CommentsListController commentsListController;
-	//private MyCommentsController myCommentsListController;
-	
+    // TODO: Refactor with new classes
+    // private CommentsListController commentsListController;
+    // private MyCommentsController myCommentsListController;
+    
+    Location currentLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
@@ -28,51 +31,96 @@ public class CreateCommentActivity extends Activity {
 
 	Button post = (Button) findViewById(R.id.create_post);
 	
-	final Context context = this;  //used to pass to LocationModel -KW
-
+	// Retrieve location manager
+	LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	
+	// Requests location from provider
+	locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+	
+	
 	post.setOnClickListener(new View.OnClickListener() {
 	    @Override
 	    public void onClick(View v) {
-	    	
-	    	EditText inputComment = (EditText) findViewById(R.id.create_text);
-	    	
-	    	/*I'm don't think that this is supposed to go here (I think we said CommentListController), 
-	    	but since the new CommentModel creation is happening here, I'm gonna put it here for now
-	    	and move it into the correct file later on -KW
-	    	*/ 
-	    	
-	    	LocationModel locMod = new LocationModel(context);
-	    	Location loc = locMod.getLocation(); // Value of loc here is null. Is onLocationChanged not being called? -KW
-	    	
-	    	
-	    	String receivedCoor = "Location is null";
-	    	if (loc!=null) {  // for testing only, since we don't have to display lat/long -KW
-	    	    double lat = loc.getLatitude();
-	    	    String strLat = String.valueOf(lat);
-	    	    double lon = loc.getLongitude();
-	    	    String strLon = String.valueOf(lon); 
-	    	    receivedCoor = "Recieved Location";
-	    	    
-	    	}
-	    	
-	    	// Refactor into MVC?
-	    	// Username is temporarily in front of the comment. Can redesign later -SB
-	    	
-	    	CommentModel comment = new CommentModel(receivedCoor+": "+inputComment.getText().toString(), null, 
-	    			CurrentUser.getName().toString() );//added a testVariable(receivedCoor) here, delete after -KW
 
-	    	ArrayList<CommentModel> commentList = ProjectApplication.getCurrentCommentList();
-	    	commentList.add(comment);
-	    	finish();
+		EditText inputComment = (EditText) findViewById(R.id.create_text);
+
+		/*
+		 * I'm don't think that this is supposed to go here (I think we
+		 * said CommentListController), but since the new CommentModel
+		 * creation is happening here, I'm gonna put it here for now and
+		 * move it into the correct file later on -KW
+		 */
+
+		String receivedCoor = "Location is null";
+		if (currentLocation != null) { // for testing only, since we don't have to
+				   // display lat/long -KW
+		    double lat = currentLocation.getLatitude();
+		    String strLat = String.valueOf(lat);
+		    double lon = currentLocation.getLongitude();
+		    String strLon = String.valueOf(lon);
+		    receivedCoor = "Recieved Location";
+
+		}
+
+		// Refactor into MVC?
+		// Username is temporarily in front of the comment. Can redesign
+		// later -SB
+
+		CommentModel comment = new CommentModel(receivedCoor + ": "
+			+ inputComment.getText().toString(), null, CurrentUser
+			.getName().toString());// added a
+					       // testVariable(receivedCoor)
+					       // here, delete after -KW
+
+		ArrayList<CommentModel> commentList = ProjectApplication
+			.getCurrentCommentList();
+		commentList.add(comment);
+		finish();
 	    }
 	});
+    
     }
+    
+	// Retrieve location updates through LocationListener interface
+	    private final LocationListener locationListener = new LocationListener() {
+
+		// TODO: override the four methods.
+		@Override
+		public void onLocationChanged(Location location) {
+
+		    if (location != null) {
+			currentLocation = location;
+			
+		    } else {
+			// do something later here
+		    }
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+
+		    // TODO
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+
+		    // TODO
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+
+		    // TODO
+		}
+	    };
+	    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    	// Inflate the menu; this adds items to the action bar if it is present.
-    	getMenuInflater().inflate(R.menu.create_comment, menu);
-    	return true;
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.create_comment, menu);
+	return true;
     }
 
 }
