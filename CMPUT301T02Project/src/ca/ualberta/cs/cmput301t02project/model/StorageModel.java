082a3d3d30091af4b95a32050b7865eb893e97f0
homynyk <homynyk@ualberta.ca>
2014-03-11 14:28:09 -0600

    Added GSON to build path

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/model/StorageModel.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/model/StorageModel.java
@@ -1,6 +1,10 @@
 package ca.ualberta.cs.cmput301t02project.model;
 
+import com.google.gson.Gson;
+
 public class StorageModel {
+	
+	private static Gson gson = new Gson();
 
 	public void storeComment(CommentModel comment) {
 	}
