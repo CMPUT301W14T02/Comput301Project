7ad921d447c906656497822cfa702cd4cc4fed2d
homynyk <homynyk@ualberta.ca>
2014-03-13 16:46:49 -0600

    Added test for viewing selected comment in BrowseReplyCommentActivityTests

--- ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseReplyCommentsActivityTest.java
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseReplyCommentsActivityTest.java
@@ -9,15 +9,56 @@ import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ViewAsserts;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class BrowseReplyCommentsActivityTest extends ActivityInstrumentationTestCase2<BrowseReplyCommentsActivity> {
 	
 	public BrowseReplyCommentsActivityTest () {
 		super(BrowseReplyCommentsActivity.class);
 	}
-
+	
 	/* Test for use case 4 */
-	public void testBrowseReplyComments() {
+	public void testDisplayComment() {
+		CommentListModel comments = new CommentListModel();
+
+		String loc = "Location Intialization";
+		Location currentLocation;
+		currentLocation = new Location(loc);
+
+		CommentModel comment = new CommentModel("comment", currentLocation, "username");
+		comments.add(comment);
+		ProjectApplication.setCurrentComment(comment);
+	
+		TextView view = (TextView) getActivity().findViewById(R.id.selected_comment);
+		BrowseReplyCommentsActivity activity = getActivity();
+		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(), view);
+		assertEquals("text should be displayed", comment.getText(), view.getText().toString());
+
+	}
+	
+	/* Test for use case 4 */
+	/*
+	public void testDisplayReplies() {
+		CommentListModel comments = new CommentListModel();
+
+		String loc = "Location Intialization";
+		Location currentLocation;
+		currentLocation = new Location(loc);
+
+		CommentModel comment = new CommentModel("comment", currentLocation, "username");
+		comments.add(comment);
+		ProjectApplication.setCurrentComment(comment);
+	
+		ListView view = (ListView) getActivity().findViewById(R.id.replyListView);
+		BrowseReplyCommentsActivity activity = getActivity();
+		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(), view);
+		assertEquals("text should be displayed", comment, view.getAdapter());
+
+	}
+	*/
+	
+	/* Test for use case 4 */
+	public void testVisibleListView() {
 		CommentListModel comments = new CommentListModel();
 
 		String loc = "Location Intialization";
