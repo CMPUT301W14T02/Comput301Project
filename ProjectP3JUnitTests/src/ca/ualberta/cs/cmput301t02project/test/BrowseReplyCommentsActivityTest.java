b110625985b20ea0749fb8d9db6c488e5e4eb062
homynyk <homynyk@ualberta.ca>
2014-03-13 16:35:36 -0600

    Added test for use case 4 to BrowseReplyCommentsActivityTest

--- ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseReplyCommentsActivityTest.java
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseReplyCommentsActivityTest.java
@@ -1,15 +1,37 @@
 package ca.ualberta.cs.cmput301t02project.test;
 
+import ca.ualberta.cs.cmput301t02project.ProjectApplication;
+import ca.ualberta.cs.cmput301t02project.R;
 import ca.ualberta.cs.cmput301t02project.activity.BrowseReplyCommentsActivity;
+import ca.ualberta.cs.cmput301t02project.model.CommentListModel;
+import ca.ualberta.cs.cmput301t02project.model.CommentModel;
+import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.widget.ListView;
 
-public class BrowseReplyCommentsActivityTest extends
-		ActivityInstrumentationTestCase2<BrowseReplyCommentsActivity> {
+public class BrowseReplyCommentsActivityTest extends ActivityInstrumentationTestCase2<BrowseReplyCommentsActivity> {
+	
+	public BrowseReplyCommentsActivityTest () {
+		super(BrowseReplyCommentsActivity.class);
+	}
 
-	public BrowseReplyCommentsActivityTest(
-			Class<BrowseReplyCommentsActivity> activityClass) {
-		super(activityClass);
-		// TODO Auto-generated constructor stub
+	/* Test for use case 4 */
+	public void testBrowseReplyComments() {
+		CommentListModel comments = new CommentListModel();
+
+		String loc = "Location Intialization";
+		Location currentLocation;
+		currentLocation = new Location(loc);
+
+		CommentModel comment = new CommentModel("comment", currentLocation, "username");
+		comments.add(comment);
+		ProjectApplication.setCurrentComment(comment);
+	
+		ListView view = (ListView) getActivity().findViewById(R.id.replyListView);
+		BrowseReplyCommentsActivity activity = getActivity();
+		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(), view);
+
 	}
 
 }
