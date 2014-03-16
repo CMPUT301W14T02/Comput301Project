6632d76e19cdac89722c9df00f8bf5cac1a931d1
kwicen <kwicento@ualberta.ca>
2014-03-16 15:59:23 -0600

    added test for adding favorite

--- ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/FavoritesControllerTest.java
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/FavoritesControllerTest.java
@@ -2,13 +2,37 @@ package ca.ualberta.cs.cmput301t02project.test;
 
 import junit.framework.TestCase;
 import ca.ualberta.cs.cmput301t02project.controller.FavoritesController;
+import ca.ualberta.cs.cmput301t02project.model.CommentListModel;
+import ca.ualberta.cs.cmput301t02project.model.CommentModel;
 
 public class FavoritesControllerTest extends TestCase {
+	
+	FavoritesController controller;
+	CommentListModel model;
 	
 	public FavoritesControllerTest(String name) {
 		super(name);
 	}
 	
+	protected void setUp() throws Exception {
+		super.setUp();
+		model = new CommentListModel();
+		controller = new FavoritesController(model);
+	}
+	
+	// Use Case 11 - KW 
+	public void favoriteCommentTest() {
+		CommentModel comment = new CommentModel("original text", null ,"username");
+		controller.favoriteComment(comment);
+		assertTrue(model.getCommentList().contains(comment));
+	}
+		
+		
+}
+	
+	
+	
+	
 	
 
-}
+
