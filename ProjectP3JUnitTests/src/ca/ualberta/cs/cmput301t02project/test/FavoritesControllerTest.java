76d1b5c5fc59a55f19cbccc8d6a56bc5061d9cda
kwicen <kwicento@ualberta.ca>
2014-03-16 15:25:17 -0600

    added test file for FavoritesController

---
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/FavoritesControllerTest.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.cmput301t02project.test;
+
+import junit.framework.TestCase;
+import ca.ualberta.cs.cmput301t02project.controller.FavoritesController;
+
+public class FavoritesControllerTest extends TestCase {
+	
+	public FavoritesControllerTest(String name) {
+		super(name);
+	}
+	
+	
+
+}
