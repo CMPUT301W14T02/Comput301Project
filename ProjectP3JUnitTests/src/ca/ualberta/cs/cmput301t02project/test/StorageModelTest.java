723948c4efa3df05b2a5eda455932c3a30d87725
kwicen <kwicento@ualberta.ca>
2014-03-16 16:29:28 -0600

    added a file for testing the storage stuff

---
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/StorageModelTest.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.cmput301t02project.test;
+
+import junit.framework.TestCase;
+
+public class StorageModelTest extends TestCase {
+	
+	public StorageModelTest(String name) {
+		super(name);
+	}
+
+	
+	
+	
+}
