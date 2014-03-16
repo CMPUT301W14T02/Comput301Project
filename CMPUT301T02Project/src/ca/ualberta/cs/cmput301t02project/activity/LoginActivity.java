f2498eda9764a5fbb895f12f45ec1607a2cbcc1b
Sasha Babicki <sbabicki@gmail.com>
2014-03-15 23:06:52 -0600

    loginActivity comments

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/LoginActivity.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/LoginActivity.java
@@ -13,14 +13,88 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+/**
+ * Allows a user to log in to the app with a selected username.
+ * If their username already exists, the previous data associated with that user is restored.
+ * If their username does not already exist, it is created.
+ */
 public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	
 		setContentView(R.layout.activity_login);
 	}
 
+	/**
+	 * Reads and relays user's username input.
+	 * <p>
+	 * Called when login_button on the screen is pressed. 
+	 * Takes a view as a parameter because of conventions for onClick in the the layout.
+	 * <p>
+	 * @param v	The current view. Variable is not used anywhere.
+	 */
+	public void submitName(View v) {
+
+		// Read user input -SB
+		EditText enteredName = (EditText) findViewById(R.id.login_username);
+
+		// Test if the input is allowed -SB
+		setUsername(enteredName.getText().toString());
+	}
+
+	/**
+	 * Sets the current user's username.
+	 * <p>
+	 * Sets the current username in ProjectApplication.
+	 * The current username is posted along with any comments the user makes. 
+	 * The user's information including old posted comments and favorite comments can be restored after the user is set. 
+	 * If an invalid username was entered, a message is printed to the screen.
+	 * <p>
+	 * @param username	The user's username input
+	 */
+	public void setUsername(String username) {
+
+		// Message on screen -SB
+		TextView message = (TextView) findViewById(R.id.login_message);
+
+		// If valid username set it -SB
+		if (checkIfValid(username)) {
+
+			// Reset message -SB
+			message.setText("Login Page");
+
+			// If user doesn't exist, create a user with that name -SB
+			UserList.findUser(username);
+
+			// Go to the main menu once user is set -SB
+			startActivity(new Intent(LoginActivity.this, MainMenuActivity.class));
+		}
+
+		// If invalid username is provided ask user to try again -SB
+		else {
+			message.setText("Please enter a valid username");
+		}
+	}
+	
+	/**
+	 * Checks if the username is valid.
+	 * <p>
+	 * If a invalid username is entered, false is returned. 
+	 * Otherwise true is returned. 
+	 * Example of an invalid username submission: "".
+	 * Example of a valid username submission: "Bob".
+	 * @param username	The username entered by the user
+	 * @return	Returns false for invalid username, else true
+	 */
+	public boolean checkIfValid(String username) {
+		if (username.equals("")) {
+			return false;
+		} 
+		return true;
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -28,47 +102,4 @@ public class LoginActivity extends Activity {
 		return true;
 	}
 
-	// accessed when login_button is pressed -SB
-	public void submitName(View v) {
-
-		// read user input -SB
-		EditText enteredName = (EditText) findViewById(R.id.login_username);
-
-		// test if the input is allowed -SB
-		setUsername(enteredName.getText().toString());
-	}
-
-	// return false for invalid usernames, true for valid ones -SB
-	public boolean checkIfValid(String username) {
-		if (username.equals("")) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	// checks if a name passed in is valid -SB
-	public void setUsername(String username) {
-
-		// message on screen -SB
-		TextView message = (TextView) findViewById(R.id.login_message);
-
-		// if valid username set it -SB
-		if (checkIfValid(username)) {
-
-			// reset message -SB
-			message.setText("Login Page");
-
-			// if user doesn't exist, create a user with that name -SB
-			UserList.findUser(username);
-
-			// go to the main menu once user is set -SB
-			startActivity(new Intent(LoginActivity.this, MainMenuActivity.class));
-		}
-
-		// if invalid username is provided ask user to try again -SB
-		else {
-			message.setText("Please enter a valid username");
-		}
-	}
 }
