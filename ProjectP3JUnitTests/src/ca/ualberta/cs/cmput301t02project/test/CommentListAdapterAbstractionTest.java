63ab31321754a1c563f421ff415a569c2de073d8
Sasha Babicki <sbabicki@gmail.com>
2014-03-16 02:38:23 -0600

    still working on sorting tests. i'm pretty sure i don't know what's supposed to be happening

--- ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/CommentListAdapterAbstractionTest.java
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/CommentListAdapterAbstractionTest.java
@@ -16,7 +16,31 @@ public class CommentListAdapterAbstractionTest extends ActivityInstrumentationTe
 		super(BrowseTopLevelCommentsActivity.class);
 	}
 	
-	public void testSortByDate() throws InterruptedException{
+	/*
+	 * Use Case 1
+	 */
+	public void testSortByLocation (){
+		
+	}
+	
+	/*
+	 * Use Case 2
+	 */
+	public void testSortByCustomLocation (){
+		
+	}
+
+	/*
+	 * Use Case 8
+	 */
+	public void testSortByPicture (){
+		
+	}
+	
+	/*
+	 * Use Case 9
+	 */
+	public void testSortByDate() {
 		CommentModel comment1 = new  CommentModel("post 1", null, "schmoop");
 		comment1.setDate(new Date(1));
 		
@@ -42,12 +66,67 @@ public class CommentListAdapterAbstractionTest extends ActivityInstrumentationTe
 		
 		adapter1 = new CommentListAdapter(getActivity(), R.layout.list_item, outOfOrderComments);
 		adapter2 = new CommentListAdapter(getActivity(), R.layout.list_item, inOrderComments);
-		adapter1.sortByDefault();
-		adapter2.sortByDefault();
+		adapter1.sortByDate();
+		adapter2.sortByDate();
 		
-		assertEquals("First items should be in same place", adapter1.getItem(0).getDate(), adapter2.getItem(0).getDate());
+		assertEquals("First item's dates should be equal", adapter1.getItem(0).getDate(), adapter2.getItem(0).getDate());
+		assertEquals("Second item's dates should be equal", adapter1.getItem(1).getDate(), adapter2.getItem(1).getDate());
+		assertEquals("Third item's dates should be equal", adapter1.getItem(2).getDate(), adapter2.getItem(2).getDate());
+		
+		assertEquals("First items should be in same place", adapter1.getItem(0), adapter2.getItem(0));
 		assertEquals("Second items should be in same place", adapter1.getItem(1), adapter2.getItem(1));
 		assertEquals("Third items should be in same place", adapter1.getItem(2), adapter2.getItem(2));
 		
 	}
+	
+	/*
+	 * Use Case 15
+	 */
+	public void testSortByRating (){
+		CommentModel comment1 = new  CommentModel("post 1", null, "schmoop");
+		comment1.setRating(0);
+		
+		CommentModel comment2 = new  CommentModel("post 2", null, "schmoop");
+		comment2.setRating(2);
+		
+		CommentModel comment3 = new  CommentModel("post 3", null, "schmoop");
+		comment3.setRating(6);
+		
+		ArrayList<CommentModel> outOfOrderComments = new ArrayList<CommentModel>();
+		outOfOrderComments.add(comment3);
+		outOfOrderComments.add(comment2);
+		outOfOrderComments.add(comment1);
+
+		
+		ArrayList<CommentModel> inOrderComments = new ArrayList<CommentModel>();
+		inOrderComments.add(comment1);
+		inOrderComments.add(comment2);
+		inOrderComments.add(comment3);
+	
+		CommentListAdapter adapter1;
+		CommentListAdapter adapter2;
+		
+		adapter1 = new CommentListAdapter(getActivity(), R.layout.list_item, outOfOrderComments);
+		adapter2 = new CommentListAdapter(getActivity(), R.layout.list_item, inOrderComments);
+		adapter1.sortByRanking();
+		adapter2.sortByRanking();
+		
+		
+		assertEquals("First items should be in same place", adapter1.getItem(0), adapter2.getItem(0));
+		assertEquals("Second items should be in same place", adapter1.getItem(1), adapter2.getItem(1));
+		assertEquals("Third items should be in same place", adapter1.getItem(2), adapter2.getItem(2));
+		
+
+		assertNotSame("First item's ratings should be equal", adapter1.getItem(0).getRating(), adapter2.getItem(0).getRating());
+		assertEquals("Second item's ratings should be equal", adapter1.getItem(1).getRating(), adapter2.getItem(1).getRating());
+		assertEquals("Third item's ratings should be equal", adapter1.getItem(2).getRating(), adapter2.getItem(2).getRating());
+	}
+	
+	/*
+	 * Use Case 16
+	 */
+	public void testSortByDefault (){
+		
+	}
+
 }
