b75d6b1b8c005c60bd82a78a0dc947024aaa3005
kwicen <kwicento@ualberta.ca>
2014-03-16 15:41:21 -0600

    added some code for BrowseFavoritesActivityTest, although throws error

--- ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseFavoritesActivityTest.java
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseFavoritesActivityTest.java
@@ -1,12 +1,31 @@
 package ca.ualberta.cs.cmput301t02project.test;
 
+import ca.ualberta.cs.cmput301t02project.ProjectApplication;
+import ca.ualberta.cs.cmput301t02project.R;
 import ca.ualberta.cs.cmput301t02project.activity.BrowseFavoritesActivity;
+import ca.ualberta.cs.cmput301t02project.model.CommentModel;
+import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
 
 public class BrowseFavoritesActivityTest extends ActivityInstrumentationTestCase2<BrowseFavoritesActivity> {
 
 	public BrowseFavoritesActivityTest() {
 		super(BrowseFavoritesActivity.class);
+	}
+	
+	public CommentModel initializeComment() {
+		String loc = "Location Intialization";
+		Location currentLocation;
+		Location myLocation;
+		currentLocation = new Location(loc);
+		myLocation = new Location(loc);
+
+		CommentModel comment = new CommentModel("comment", currentLocation, "username");
+
+		ProjectApplication.setCurrentLocation(myLocation);
+
+		return comment;
 	}
 
 	public void testDisplayFavorites() {
@@ -15,7 +34,13 @@ public class BrowseFavoritesActivityTest extends ActivityInstrumentationTestCase
 
 	/* Test for use case 21 */
 	public void testDisplayUsername() {
-		assertTrue(false);
+		//Currently throws an error -KW
+		CommentModel comment = initializeComment();
+
+		ListView view = (ListView) getActivity().findViewById(R.id.commentListView);
+		assertTrue("username should be displayed", view.getAdapter().getItem(0).toString().contains(comment.getUsername()));
+
+		
 
 	}
 
