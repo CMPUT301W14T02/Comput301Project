ed6a7fb8ddb70d64be869b959eb62ad3ade8cac1
Sasha Babicki <sbabicki@gmail.com>
2014-03-16 03:08:42 -0600

    i need to go to sleep now. sorting tests are still not passing, so i will have to deal with this tomorrow.

--- ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/CommentListAdapterAbstractionTest.java
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/CommentListAdapterAbstractionTest.java
@@ -3,6 +3,7 @@ package ca.ualberta.cs.cmput301t02project.test;
 import java.util.ArrayList;
 import java.util.Date;
 
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
 import ca.ualberta.cs.cmput301t02project.R;
@@ -119,6 +120,45 @@ public class CommentListAdapterAbstractionTest extends ActivityInstrumentationTe
 	 */
 	public void testSortByPicture (){
 		
+		Bitmap pic = null;
+		
+		// Has picture -SB
+		CommentModel comment1 = new  CommentModel("post 1", pic, null, "schmoop");
+		
+		// Does not have picture -SB
+		CommentModel comment2 = new  CommentModel("post 2", null, "schmoop");
+
+		// Has picture -SB
+		CommentModel comment3 = new  CommentModel("post 3", pic, null, "schmoop");
+		
+		//  Does not have picture -SB
+		CommentModel comment4 = new  CommentModel("post 4", null, "schmoop");
+		
+		ArrayList<CommentModel> outOfOrderComments = new ArrayList<CommentModel>();
+		outOfOrderComments.add(comment1);
+		outOfOrderComments.add(comment2);
+		outOfOrderComments.add(comment3);
+		outOfOrderComments.add(comment4);
+	
+		ArrayList<CommentModel> inOrderComments = new ArrayList<CommentModel>();
+		inOrderComments.add(comment1);
+		inOrderComments.add(comment3);
+		inOrderComments.add(comment2);
+		inOrderComments.add(comment4);
+	
+		CommentListAdapter adapter1;
+		CommentListAdapter adapter2;
+		
+		adapter1 = new CommentListAdapter(getActivity(), R.layout.list_item, outOfOrderComments);
+		adapter2 = new CommentListAdapter(getActivity(), R.layout.list_item, inOrderComments);
+		
+		adapter1.sortByPicture();
+		adapter2.sortByPicture();
+		
+		assertEquals("First items should be in same place", adapter1.getItem(0), adapter2.getItem(0));
+		assertEquals("Second items should be in same place", adapter1.getItem(1), adapter2.getItem(1));
+		assertEquals("Third items should be in same place", adapter1.getItem(2), adapter2.getItem(2));
+		assertEquals("Forth items should be in same place", adapter1.getItem(3), adapter2.getItem(3));
 	}
 	
 	/*
@@ -209,7 +249,41 @@ public class CommentListAdapterAbstractionTest extends ActivityInstrumentationTe
 	 * Use Case 16
 	 */
 	public void testSortByDefault (){
+		CommentModel comment1 = new  CommentModel("post 1", null, "schmoop");
+		comment1.setDate(new Date(1));
 		
+		CommentModel comment2 = new  CommentModel("post 2", null, "schmoop");
+		comment2.setDate(new Date(20000));
+		
+		CommentModel comment3 = new  CommentModel("post 3", null, "schmoop");
+		comment3.setDate(new Date(300000000));
+		
+		ArrayList<CommentModel> outOfOrderComments = new ArrayList<CommentModel>();
+		outOfOrderComments.add(comment3);
+		outOfOrderComments.add(comment2);
+		outOfOrderComments.add(comment1);
+
+		
+		ArrayList<CommentModel> inOrderComments = new ArrayList<CommentModel>();
+		inOrderComments.add(comment1);
+		inOrderComments.add(comment2);
+		inOrderComments.add(comment3);
+	
+		CommentListAdapter adapter1;
+		CommentListAdapter adapter2;
+		
+		adapter1 = new CommentListAdapter(getActivity(), R.layout.list_item, outOfOrderComments);
+		adapter2 = new CommentListAdapter(getActivity(), R.layout.list_item, inOrderComments);
+		adapter1.sortByDefault();
+		adapter2.sortByDefault();
+		
+		assertEquals("First items should be in same place", adapter1.getItem(0), adapter2.getItem(0));
+		assertEquals("Second items should be in same place", adapter1.getItem(1), adapter2.getItem(1));
+		assertEquals("Third items should be in same place", adapter1.getItem(2), adapter2.getItem(2));
+
+		assertEquals("First item's dates should be equal", adapter1.getItem(0).getDate(), adapter2.getItem(0).getDate());
+		assertEquals("Second item's dates should be equal", adapter1.getItem(1).getDate(), adapter2.getItem(1).getDate());
+		assertEquals("Third item's dates should be equal", adapter1.getItem(2).getDate(), adapter2.getItem(2).getDate());	
 	}
 
 }
