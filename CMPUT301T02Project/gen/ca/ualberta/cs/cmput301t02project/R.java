83c4de1fa16edd4383824375d02f1e3623420b16
Sasha Babicki <sbabicki@gmail.com>
2014-03-17 03:21:56 -0600

    got rid of menu items since we aren't using them yet.

--- CMPUT301T02Project/gen/ca/ualberta/cs/cmput301t02project/R.java
+++ CMPUT301T02Project/gen/ca/ualberta/cs/cmput301t02project/R.java
@@ -24,7 +24,6 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080013;
         public static final int browse=0x7f08000a;
         public static final int commentListView=0x7f080004;
         public static final int create=0x7f080009;
