119da010b9ca613c5ed80f18da6793bf48d43371
homynyk <homynyk@ualberta.ca>
2014-03-29 14:12:45 -0600

    Caching myComments is working

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/model/StorageModel.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/model/StorageModel.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -15,6 +16,7 @@ import android.location.Location;
 import ca.ualberta.cs.cmput301t02project.ProjectApplication;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * Deals with storing data on the server and caching data locally.
@@ -52,11 +54,14 @@ public class StorageModel {
 			System.out.println("Open file");
 			FileInputStream fis = context.openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				myCommentsArray.add(CommentModel.fromJSON(line));
-				line = in.readLine();
-			}
+			
+			Type type = new TypeToken<ArrayList<CommentModel>>(){}.getType();
+			myCommentsArray = gson.fromJson(in, type);
+			//String line = in.readLine();
+			//while (line != null) {
+				//myCommentsArray.add(gson.fromJson(line, CommentModel.class));
+				//line = in.readLine();
+			//}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
