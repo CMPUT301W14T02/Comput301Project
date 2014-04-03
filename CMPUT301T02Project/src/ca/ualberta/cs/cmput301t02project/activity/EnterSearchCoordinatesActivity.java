d5992ce364721fc30bac727ae97b84ab3adece45
kwicen <kwicento@ualberta.ca>
2014-04-03 15:37:58 -0600

    tried the dialog thing

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/EnterSearchCoordinatesActivity.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/EnterSearchCoordinatesActivity.java
@@ -1,23 +1,44 @@
 package ca.ualberta.cs.cmput301t02project.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 import ca.ualberta.cs.cmput301t02project.R;
-import ca.ualberta.cs.cmput301t02project.model.User;
 
+/* code adapted from http://developer.android.com/guide/topics/ui/dialogs.html- accessed April 3 2014 */
 public class EnterSearchCoordinatesActivity extends DialogFragment {
-	
-	//public Dialog onCreateDialog(Bundle savedInstanceState) {
-		//super.onCreate(savedInstanceState);
-		//setContentView(R.layout.enter_coordinates);
-		
-	//}
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// super.onCreate(savedInstanceState);
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		// Get the layout inflater
+		LayoutInflater inflater = getActivity().getLayoutInflater();
 
-	
+		// Inflate and set the layout for the dialog
+		// Pass null as the parent view because its going in the dialog layout
+		builder.setView(inflater.inflate(R.layout.enter_coordinates, null))
+				// Add action buttons
+				.setPositiveButton(R.string.sort_button,
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int id) {
+								// sort the comments
+							}
+						})
+				.setNegativeButton(R.string.cancel_button,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								EnterSearchCoordinatesActivity.this.getDialog().cancel();
+							}
+						});
+		return builder.create();
+	}
 }
