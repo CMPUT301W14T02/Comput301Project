ec35f48335039f0205bf749a3f47cf72d31bfe01
rooshilp <rooshilp@gmail.com>
2014-04-03 14:13:45 -0600

    added more junit tests

---
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/CommentControllerTest.java
@@ -0,0 +1,7 @@
+package ca.ualberta.cs.cmput301t02project.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class CommentControllerTest extends ActivityInstrumentationTestCase2<T> {
+
+}
---
+++ ProjectP3JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/GPSLocationTest.java
@@ -0,0 +1,7 @@
+package ca.ualberta.cs.cmput301t02project.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class GPSLocationTest extends ActivityInstrumentationTestCase2<T> {
+
+}
