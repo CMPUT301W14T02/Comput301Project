5ed7723981c2114ccf4e60babfb3e3d791afa72f
Sasha Babicki <sbabicki@gmail.com>
2014-04-03 12:23:26 -0600

    moved things around so that the picture stuff is contained in a different class. is it bad practice to not have an onCreate override for things that extend activity?

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/CreateCommentActivity.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/CreateCommentActivity.java
@@ -22,11 +22,10 @@ import ca.ualberta.cs.cmput301t02project.model.User;
 
 /**
  * Allows a user to create their own comment. 
+ * picture is defined in PictureActivity, which deals with all the image related tasks.
  */
-public class CreateCommentActivity extends ActionBarActivity {
+public class CreateCommentActivity extends PictureActivity {
 	
-	// if no picture is taken, picture should remain null -SB
-	Bitmap picture = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,50 +82,7 @@ public class CreateCommentActivity extends ActionBarActivity {
 		});
 
 	}
-	
-	
-	/**
-	 * Takes a photo using the android camera
-	 * <p>
-	 * * Taken directly from the CameraTest project on eclass (accessed April 1, 2014) *
-	 * Sets up a file to store the photo.
-	 * Takes the photo and allows the user to retake it if needed.
-	 * Returns to this activity when done.
-	 * <p>
-	 * @param v	to allow takePhoto to be called using XML, view is a required parameter
-	 */
-	public void takePhoto(View v) {
 
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		
-		String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/savefile.jpg";
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, path);
-		startActivityForResult(intent, 0);
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		
-		//String path = data.getStringExtra(MediaStore.EXTRA_OUTPUT);
-		ImageButton imageButton = (ImageButton) findViewById(R.id.takeAPhoto);
-		
-		if(requestCode == 0) {
-			if(resultCode == RESULT_OK) {
-				
-				// retrieve the bitmap -SB
-				Bitmap bm = (Bitmap) data.getExtras().getParcelable("data");
-				
-				// display the image on the image button -SB
-				imageButton.setImageBitmap(bm);
-				
-				// set the picture for the comment to the bitmap -SB
-				picture = bm;
-			}
-			else {
-				//something went wrong
-			}
-		}
-	}	
 
 	@Override
 	public void goToHelpPage(){
---
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/PictureActivity.java
@@ -0,0 +1,59 @@
+package ca.ualberta.cs.cmput301t02project.activity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import ca.ualberta.cs.cmput301t02project.R;
+
+public class PictureActivity extends ActionBarActivity {
+
+	// if no picture is taken, picture should remain null -SB
+	Bitmap picture = null;
+	
+	/**
+	 * Takes a photo using the android camera
+	 * <p>
+	 * * Taken directly from the CameraTest project on eclass (accessed April 1, 2014) *
+	 * Sets up a file to store the photo.
+	 * Takes the photo and allows the user to retake it if needed.
+	 * Returns to this activity when done.
+	 * <p>
+	 * @param v	to allow takePhoto to be called using XML, view is a required parameter
+	 */
+	public void takePhoto(View v) {
+
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		
+		String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/savefile.jpg";
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, path);
+		startActivityForResult(intent, 0);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		
+		//String path = data.getStringExtra(MediaStore.EXTRA_OUTPUT);
+		ImageButton imageButton = (ImageButton) findViewById(R.id.takeAPhoto);
+		
+		if(requestCode == 0) {
+			if(resultCode == RESULT_OK) {
+				
+				// retrieve the bitmap -SB
+				Bitmap bm = (Bitmap) data.getExtras().getParcelable("data");
+				
+				// display the image on the image button -SB
+				imageButton.setImageBitmap(bm);
+				
+				// set the picture for the comment to the bitmap -SB
+				picture = bm;
+			}
+			else {
+				//something went wrong
+			}
+		}
+	}	
+
+}
