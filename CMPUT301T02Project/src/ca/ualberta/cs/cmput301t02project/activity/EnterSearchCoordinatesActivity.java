1adda3018cf919dcc70c0e0295a47f64691b3503
kwicen <kwicento@ualberta.ca>
2014-04-03 15:49:08 -0600

    dropped the dialog thing, converted to reg activity

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/EnterSearchCoordinatesActivity.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/EnterSearchCoordinatesActivity.java
@@ -13,32 +13,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import ca.ualberta.cs.cmput301t02project.R;
 
-/* code adapted from http://developer.android.com/guide/topics/ui/dialogs.html- accessed April 3 2014 */
-public class EnterSearchCoordinatesActivity extends DialogFragment {
+public class EnterSearchCoordinatesActivity extends Activity {
 	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		// super.onCreate(savedInstanceState);
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		// Get the layout inflater
-		LayoutInflater inflater = getActivity().getLayoutInflater();
-
-		// Inflate and set the layout for the dialog
-		// Pass null as the parent view because its going in the dialog layout
-		builder.setView(inflater.inflate(R.layout.enter_coordinates, null))
-				// Add action buttons
-				.setPositiveButton(R.string.sort_button,
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int id) {
-								// sort the comments
-							}
-						})
-				.setNegativeButton(R.string.cancel_button,
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								EnterSearchCoordinatesActivity.this.getDialog().cancel();
-							}
-						});
-		return builder.create();
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.enter_coordinates);
 	}
 }
