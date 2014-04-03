14870555b900602602d48e502a4d31c61209978d
sbabicki <sbabicki@gmail.com>
2014-04-02 20:43:49 -0600

    also, should have mentioned in last commit: edit comment now working from menu

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/BrowseReplyCommentsActivity.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/BrowseReplyCommentsActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
