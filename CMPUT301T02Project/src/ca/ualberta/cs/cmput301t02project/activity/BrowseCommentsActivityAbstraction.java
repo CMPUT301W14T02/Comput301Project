d20d06c6db89495a7e84a345df8699520b1ce99e
sbabicki <sbabicki@gmail.com>
2014-04-02 19:47:23 -0600

    made new class to fix god class

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/BrowseCommentsActivityAbstraction.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/activity/BrowseCommentsActivityAbstraction.java
@@ -20,10 +20,9 @@ import ca.ualberta.cs.cmput301t02project.view.CommentListAdapterAbstraction;
  * Contains common code inherited by activities that browse and sort comments, including 
  * BrowseMyCommmentsActivity, BrowseReplyCommentsActivity, BrowseFavoritesActivity, BrowseTopLevelCommentsActivtiy.
  * Includes methods for 
- * displaying a drop-down menu of sorting options, 
- * setting up an action-bar menu and dealing with menu items being clicked
+ * displaying a drop-down menu of sorting options
  */
-public abstract class BrowseCommentsActivityAbstraction extends Activity implements OnItemSelectedListener{
+public abstract class BrowseCommentsActivityAbstraction extends ActionBarActivity implements OnItemSelectedListener{
 	
 	// The adapter used by the class that extends BrowseCommentsActivityAbstraction -SB
 	protected CommentListAdapterAbstraction adapter;
@@ -38,17 +37,6 @@ public abstract class BrowseCommentsActivityAbstraction extends Activity impleme
 	 */
 	public abstract CommentListAdapterAbstraction initializeAdapter();
 	
-	/**
-	 * Redirects to the help page for that activity
-	 * <p>
-	 * If goToHelpPage method is not re-implemented in a class that extends this class then default help page is used.
-	 * Any classes that extend BrowseCommentsActivityAbstraction should override this method to set their own specific help page.
-	 * All the Browse* classes override this method to set their own specific help page
-	 * <p>
-	 */
-	public void goToHelpPage(){
-		// default help page -SB
-	}
 	
 	/**
 	 * Performs tasks to set up the activity including creating a sorting menu and initializing the adapter.
@@ -64,61 +52,8 @@ public abstract class BrowseCommentsActivityAbstraction extends Activity impleme
 		adapter = initializeAdapter();
 		listView.setAdapter(adapter);
 	}
-	
-	/**
-	 * Return to the login screen.
-	 * <p>
-	 * Redirects page to the login page (LoginActivity).
-	 * Currently does not unset the User. 
-	 * Called by any classes with a logout button on their menu that extend BrowseCommentsActivityAbstraction, 
-	 * a.k.a. all the Browse* activities.
-	 * <p>
-	 */
-	public void logout(){
-		Intent logoutActivity = new Intent(getApplicationContext(),LoginActivity.class);
-		startActivity(logoutActivity);
-	}
-	
-	/**
-	 * Return to the main menu (homepage).
-	 * <p>
-	 * Redirects page to the homepage (MainMenuActivity).
-	 * Called by any classes with a home button on their menu that extend BrowseCommentsActivityAbstraction, 
-	 * a.k.a. all the Browse* activities.
-	 * <p>
-	 */
-	public void returnToHomepage(){
-		Intent goHome = new Intent(getApplicationContext(),MainMenuActivity.class);
-		startActivity(goHome);
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.comments_list, menu);
-		
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    
-	    switch (item.getItemId()) {
-	    	case R.id.help_page:
-	    		goToHelpPage();
-	    		return true;
-	    	case R.id.home:
-	    		returnToHomepage();
-	    		return true;
-	        case R.id.logout:
-	            logout();
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}
-	
+
 	/**
 	 * Creates a drop-down menu of sorting options.
 	 * <p>
