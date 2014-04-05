4646558fdefb60601eb1aa7def3e44e8d96a2d80
Sasha Babicki <sbabicki@gmail.com>
2014-04-05 12:05:15 -0600

    re-installed eclipse and am going to start writing test cases for the new requirement. eclipse seems to be working for now...

---
+++ ProjectP4JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseFollowedCommentsActivityTest.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cs.cmput301t02project.test;
+
+import ca.ualberta.cs.cmput301t02project.activity.BrowseFavoritesActivity;
+import ca.ualberta.cs.cmput301t02project.activity.BrowseFollowedCommentsActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class BrowseFollowedCommentsActivityTest extends ActivityInstrumentationTestCase2<BrowseFollowedCommentsActivity> {
+
+	public BrowseFollowedCommentsActivityTest() {
+		super(BrowseFollowedCommentsActivity.class);
+	}
+}
