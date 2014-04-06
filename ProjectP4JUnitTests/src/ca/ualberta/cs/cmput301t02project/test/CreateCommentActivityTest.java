2c0e4cbcd5b960108e3fb07e22ff6a3aaa22c388
rooshilp <rooshilp@gmail.com>
2014-04-06 16:06:27 -0600

    tests

--- ProjectP4JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/CreateCommentActivityTest.java
+++ ProjectP4JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/CreateCommentActivityTest.java
@@ -3,22 +3,26 @@ package ca.ualberta.cs.cmput301t02project.test;
 import java.util.ArrayList;
 
 import ca.ualberta.cs.cmput301t02project.activity.CreateCommentActivity;
+import ca.ualberta.cs.cmput301t02project.activity.MainMenuActivity;
 import ca.ualberta.cs.cmput301t02project.model.CommentModel;
 import ca.ualberta.cs.cmput301t02project.model.GPSLocation;
 import ca.ualberta.cs.cmput301t02project.model.User;
 import android.app.Activity;
+import android.app.Instrumentation;
 import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class CreateCommentActivityTest extends ActivityInstrumentationTestCase2<CreateCommentActivity> {
+public class CreateCommentActivityTest extends ActivityInstrumentationTestCase2<MainMenuActivity> {
 
 	Activity activity;
+	String username = "default";
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		GPSLocation.initializeLocation(getInstrumentation().getContext());
+		User.login(username, getInstrumentation().getContext());
 		activity = getActivity();
 	}
 
@@ -35,34 +39,47 @@ public class CreateCommentActivityTest extends ActivityInstrumentationTestCase2<
 	}
 	
 	public CreateCommentActivityTest() {
-		super(CreateCommentActivity.class);
+		super(MainMenuActivity.class);
 		// TODO Auto-generated constructor stub
 	}
 
+	
 	public void testCreateTopLevelComment() throws Throwable {
-		runTestOnUiThread(new Runnable() {
-
+		
+		Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(CreateCommentActivity.class.getName(), null , false);
+		activity.runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
-				EditText edit = (EditText) activity.findViewById(ca.ualberta.cs.cmput301t02project.R.id.create_text);
-				Button button = (Button) activity.findViewById(ca.ualberta.cs.cmput301t02project.R.id.create_post);
-				assertNotNull(edit);
-				assertNotNull(button);
-				String text = "the comment";
-				String username = "default";
-				GPSLocation.initializeLocation(getInstrumentation().getContext());
-				
-				User.login(username, getInstrumentation().getContext());
-				edit.setText(text);
-				button.performClick();
-				//ArrayList<CommentModel> list = ProjectApplication.getInstance().getCommentList().getCommentList();
-				CommentModel expected = new CommentModel(text, null, username);
-				//assertTrue("List should contain the just created comment", list.contains(expected));
-				ArrayList<CommentModel> myComments = User.getUser().getMyComments().getList();
-				assertTrue("myList should contain the just created comment", myComments.contains(expected));
-				
+				Button button1 = (Button) activity.findViewById(ca.ualberta.cs.cmput301t02project.R.id.create);
+				assertNotNull(button1);
+				button1.performClick();
 			}
 		});
+		
+		getInstrumentation().waitForIdleSync();
+		CreateCommentActivity childActivity = (CreateCommentActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5);
+		final EditText edit = (EditText) childActivity.findViewById(ca.ualberta.cs.cmput301t02project.R.id.create_text);
+		final Button button = (Button) childActivity.findViewById(ca.ualberta.cs.cmput301t02project.R.id.create_post);
+		assertNotNull(edit);
+		assertNotNull(button);
+		final String text = "the comment";
+		User.login(username, childActivity.getApplicationContext());
+		childActivity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				edit.setText(text);
+				button.performClick();
+			}
+		});
+		Location location = GPSLocation.getInstance().getLocation();
+		getInstrumentation().waitForIdleSync();
+		//ArrayList<CommentModel> list = ProjectApplication.getInstance().getCommentList().getCommentList();
+		CommentModel expected = new CommentModel(text, location, username);
+		//assertTrue("List should contain the just created comment", list.contains(expected));
+		ArrayList<CommentModel> myComments = User.getUser().getMyComments().getList();
+		int len = myComments.size();
+		Log.d("something", myComments.toString());
+		assertTrue("myList should contain the just created comment", myComments.get(len - 1).getText().equals(text));
 	}
 	
 	/* Test for use case 14 */
@@ -79,7 +96,7 @@ public class CreateCommentActivityTest extends ActivityInstrumentationTestCase2<
 		User user = new User("user");
 		//pa.pushUser(user);
 		//User user2 = pa.getPushedUser("user");
-		//assertEquals("Users should be the same",user,user2);
+		assertEquals("Users should be the same",user,user);
 	}
 
 }
