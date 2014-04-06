56a9abdd24b9843281cfd200bca2df2184417c11
kwicen <kwicento@ualberta.ca>
2014-04-05 19:31:54 -0600

    added file to put custom location tests

---
+++ ProjectP4JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseCommentsActivityAbstractionTest.java
@@ -0,0 +1,32 @@
+package ca.ualberta.cs.cmput301t02project.test;
+
+import ca.ualberta.cs.cmput301t02project.activity.BrowseCommentsActivityAbstraction;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class BrowseCommentsActivityAbstractionTest extends
+		ActivityInstrumentationTestCase2<BrowseCommentsActivityAbstraction> {
+	
+	public BrowseCommentsActivityAbstractionTest() {
+		super(BrowseCommentsActivityAbstraction.class);
+	}
+
+	// tests for use case 2
+	
+	public void testGetCoordinates() {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
