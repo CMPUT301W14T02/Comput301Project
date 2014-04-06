a053bde67cad336f01a4b1da67b9de607921f995
kwicen <kwicento@ualberta.ca>
2014-04-05 20:19:08 -0600

    added the name of test to browse abstaction test file

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/view/CommentListAdapter.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/view/CommentListAdapter.java
@@ -385,4 +385,5 @@ public class CommentListAdapter extends ArrayAdapter<CommentModel> implements Ob
 		this.otherLocation = otherLocation;
 	}
 	
+	
 }
--- ProjectP4JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseCommentsActivityAbstractionTest.java
+++ ProjectP4JUnitTests/src/ca/ualberta/cs/cmput301t02project/test/BrowseCommentsActivityAbstractionTest.java
@@ -1,6 +1,12 @@
 package ca.ualberta.cs.cmput301t02project.test;
 
+import ca.ualberta.cs.cmput301t02project.R;
 import ca.ualberta.cs.cmput301t02project.activity.BrowseCommentsActivityAbstraction;
+import ca.ualberta.cs.cmput301t02project.activity.EnterSearchCoordinatesActivity;
+import ca.ualberta.cs.cmput301t02project.model.TopLevelCommentList;
+import ca.ualberta.cs.cmput301t02project.view.CommentListAdapter;
+import android.content.Intent;
+import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class BrowseCommentsActivityAbstractionTest extends
@@ -12,7 +18,8 @@ public class BrowseCommentsActivityAbstractionTest extends
 
 	// tests for use case 2
 	
-	public void testGetCoordinates() {
+	public void testSortByOtherLocation() {
+		
 		
 		
 	}
