6cae5943a2a07a2dfba2349afc75f0639fa7cc8d
Sasha Babicki <sbabicki@gmail.com>
2014-04-06 01:44:42 -0600

    Done commenting the rest of the classes. Deleted some unused commented out code in User. Hopefully no one needed that. I am officially done with writing javadoc documentation.

--- CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/model/User.java
+++ CMPUT301T02Project/src/ca/ualberta/cs/cmput301t02project/model/User.java
@@ -10,7 +10,11 @@ import android.content.Context;
 /**
  * The User class stores information about a user. 
  * The information stored includes 
- * the username of the user, a list of their comments, and a list of their favorite comments.
+ * the username of the user, 
+ * a list of their comments,
+ * a list of their favorite comments and their replies,
+ * a list of comments by their followed users,
+ * and a list of followed user's usernames.
  */
 public class User {
 
@@ -23,29 +27,49 @@ public class User {
 	private transient RepliesToFavsListModel repliesToFavs;
 	private transient FollowedUserCommentsListModel followedUsersComments;
 	private transient ArrayList<String> followedUsernames = new ArrayList<String>();
-	
 
 	//Singleton
 	private static User user;
 
+	/**
+	 * Create a new user with a specific username
+	 * @param username	the desired name for the user
+	 */
 	public User(String username) {
 		this.username = username.toLowerCase();
 	}
 	
+	/**
+	 * Sets the current user and retrieves their lists.
+	 * <p>
+	 * The server is checked for a user with the specified username.
+	 * If the username exists, the users old information is used.
+	 * Otherwise a new user is created.
+	 * Called from LoginActivity.
+	 * <p>
+	 * @param username	the username that the user wants to log with
+	 * @param context	the context of the calling class
+	 */
 	public static void login(String username, Context context) {
 		Server server = new Server(context);
 		user = new User(username);
 		server.pullUser(user);
+		
 		if(user.getId() == null) {
 			user.myCommentsIds = new ArrayList<String>();
 			server.postUser(user);
 		}
+		
 		user.myComments = MyCommentsListModel.getInstance(context);
 		user.favorites = FavoritesListModel.getInstance(context);
 		user.repliesToFavs = RepliesToFavsListModel.getInstance(context);
 		user.followedUsersComments = FollowedUserCommentsListModel.getInstance(context);
 	}
 	
+	/**
+	 * Returns the user that is currently logged in.
+	 * @return	the current user if it is set, otherwise throws exception
+	 */
 	public static User getUser() {
 		if(user == null) {
 			throw new IllegalAccessError("Can't get user before logging in");
@@ -60,7 +84,6 @@ public class User {
 	 * <p>
 	 * @param username	The username for the User
 	 */
-
 	public void setName(String username) {
 		this.username = username.toLowerCase();
 	}
@@ -83,26 +106,26 @@ public class User {
 	/**
 	 * Returns a list of comments composed by a User. 
 	 * <p>
-	 * The user's comments (CommentModels) are stored in a CommentListModel.
+	 * The user's comments (CommentModels) are stored in a MyCommentListModel.
 	 * The user's comments are retrieved in the 
 	 * EditCommentActivity (in order to be edited) and BrowseMyCommentsActivity (in order to be displayed).
 	 * <p>
 	 * @return	List of comments that the User posted
-	 * @see CommentListModel	Example of a CommentListModel
+	 * @see MyCommentListModel	Example of a MyCommentListModel
 	 */
 	public MyCommentsListModel getMyComments() {
 		return myComments;
 	}
 	
 	/**
-	 * Stores comment on phone storage
+	 * Adds a comment to the user's list of their own comments and stores the updated user information on the server.
 	 * <p>
-	 * Stores comment created by user to phone by calling 
-	 * storeMyComment method in StorageModel class
-	 * CreateCommentActivity
+	 * Called by CreateCommentActivity when a new comment is posted. 
+	 * Stores the updated user information containing the comment in the User class.
+	 * Also pushes updated user information containing the comment to the server.
 	 * <p>
-	 * @param comment - comment to be stored
-	 * @param context - context of the application
+	 * @param comment	the new comment to be stored
+	 * @param context	context of the application
 	 */
 	public void addMyComment(CommentModel comment, Context context) {
 		Server server = new Server(context);
@@ -112,66 +135,35 @@ public class User {
 	}
 
 	/**
-	 * Stores comment on phone storage
+	 * Adds a comment to the user's list of favorite comments.
 	 * <p>
-	 * Stores comment favorited by user to phone by calling 
-	 * storeFavorite method in StorageModel class
-	 * BrowseRepliesActivity
+	 * Called from BrowseReplyCommentActivity when the "Add to Favorites" button is selected.
+	 * Updates the user information in the User class. 
 	 * <p>
-	 * @param comment - comment to be stored
+	 * @param comment	the new comment to be stored
 	 */
 	public void addFavoriteComment(CommentModel comment, ArrayList<CommentModel> replies) {
 		favorites.addFavoriteComment(comment, replies, repliesToFavs);
 	}
 	
 	/**
-	 * Adds the comment of a followed user
-	 * @param comment	the comment to be stored in followedUsers
+	 * Stores the username of the comment author as a followed user.
+	 * <p>
+	 * Called from BrowseReplyCommentActivity when the "Follow" button is selected. 
+	 * The real interest is in the username of the comment author, 
+	 * which is extracted from the comment and stored in an ArrayList of strings called followedUsernames. 
+	 * <p>
+	 * @param comment	the current comment when the "Follow" button is pressed
 	 */
 	public void addFollowedUser(CommentModel comment){
-		// followedUsersComments.add(comment);
+		
 		if(!followedUsernames.contains(comment.getUsername())){
 			followedUsernames.add(comment.getUsername());
 		}
 	}
 	
-	
-	/**
-	 * Returns list of comments from phone storage
-	 * <p>
-	 * Retrieves replies of comments favorited by current user from phone storage 
-	 * by calling retrieveFavorites method in StorageModel class
-	 * BrowseFavoritesActivity
-	 * <p>
-	 * @param context - context of the application
-	 * @param CommentModel - favorited comment selected by the user
-	 * @return CommentListModel containing comments favorited by user
-	 *
-	public CommentListModel getFavoriteReplies(Context context, CommentModel favComment){
-		CommentListModel replies = new CommentListModel();
-		ArrayList<ArrayList<CommentModel>> favoritesArray;
-		favoritesArray = store.retrieveFavorites(context);
-		if (favoritesArray == null)
-			 favoritesArray = new ArrayList<ArrayList<CommentModel>>();
-		for (int i=0; i<favoritesArray.size();i++){
-			if (favoritesArray.get(i).get(0).getId().matches(favComment.getId())){
-				for (int j=1; j<favoritesArray.get(i).size();j++){
-					replies.add(favoritesArray.get(i).get(j));
-				}
-			}
-		}
-		return replies;
-	}*/
-	
-	/**
-	 * Returns list of comments from phone storage
-	 * <p>
-	 * Retrieves comments favorited by current user from phone storage 
-	 * by calling retrieveFavorites method in StorageModel class
-	 * BrowseFavoritesActivity
-	 * <p>
-	 * @param context - context of the application
-	 * @return CommentListModel containing comments favorited by user
+	/*
+	 * GETTERS AND SETTERS BELOW
 	 */
 	public FavoritesListModel getFavorites(){
 		return favorites;
@@ -201,8 +193,12 @@ public class User {
 		this.myCommentsIds = myCommentsIds;
 	}
 	
+	/**
+	 * Pull and return new comments from followed users on the server.
+	 * @return	updated list of followed user comments
+	 */
 	public FollowedUserCommentsListModel getFollowedUsers() {
-		//return followedUsersComments;
+		
 		if (followedUsernames.size()>0){
 			return followedUsersComments.updated(followedUsernames);
 		}
@@ -214,5 +210,4 @@ public class User {
 	public void setFollowedUsers(FollowedUserCommentsListModel followedUsers) {
 		this.followedUsersComments = followedUsers;
 	}
-	
 }
